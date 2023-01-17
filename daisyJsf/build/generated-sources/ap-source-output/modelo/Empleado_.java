ea33a8567221ff4fd70fa53f5f23454a1649e741
ABYGAIL1410 <daysizhingri@gmail.com>
2023-01-16 23:36:05 -0500

    Proyecto Web Subido

---
+++ daisyJsf/build/generated-sources/ap-source-output/modelo/Empleado_.java
@@ -0,0 +1,16 @@
+package modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import modelo.Departamento;
+
+@Generated(value="EclipseLink-2.7.4.v20190115-rNA", date="2023-01-16T19:41:02")
+@StaticMetamodel(Empleado.class)
+public class Empleado_ { 
+
+    public static volatile SingularAttribute<Empleado, Departamento> idDepartamento;
+    public static volatile SingularAttribute<Empleado, Integer> idEmpleado;
+    public static volatile SingularAttribute<Empleado, String> nombre;
+
+}
